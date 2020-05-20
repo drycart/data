8f8c2e2dc9ea3ae56e9345b7d6878a5feb8f9940
mendel <mendel@zzzlab.com>
2020-05-21 00:05:47 +0300

    Update tests

--- tests/MetadataTest.php
+++ tests/MetadataTest.php
@@ -16,13 +16,22 @@ class MetadataTest extends \PHPUnit\Framework\TestCase
         $helper = new MetaData(
             dummy\DummyExtendedModel::class,
             ['@var'=>'var','@param'=>'param', '@return'=>'return']);
-        $this->assertEquals(
-            $helper->fields(),
-            [
-                'name'=>['var'=>[['string','name']]],
-                'age'=>['var'=>[['int','age']]]
-            ]
-        );
+        $fields = $helper->fields();
+        $this->assertIsArray($fields);
+        $this->assertCount(2, $fields);
+        $this->assertArrayHasKey('name', $fields);
+        $this->assertArrayHasKey('age', $fields);
+        
+        $this->assertIsArray($fields['name']);
+        $this->assertCount(1, $fields['name']);
+        $this->assertArrayHasKey('var', $fields['name']);
+        $this->assertEquals([['string','name']],$fields['name']['var']);
+        
+        $this->assertIsArray($fields['age']);
+        $this->assertCount(1, $fields['age']);
+        $this->assertArrayHasKey('var', $fields['age']);
+        $this->assertEquals([['int','age']],$fields['age']['var']);
+        
         $this->assertEquals(
             $helper->methods(),
             [
